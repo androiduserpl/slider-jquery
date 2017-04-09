0f98052256f5434e88d4dea144d448607233d16a
Adrian Nowakowski <androiduserpl@gmail.com>
2017-04-09 12:27:37 +0200

    init 5

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
     'use strict';
-    
+
     //zmienne:
     var slider = $('#slider');
     var slideShow = $('.slide-show');
@@ -8,35 +8,62 @@ $(function(){
     var slideWidth = 100 / slideCount;
     var slideIndex = 0;
     
-    
+    var prevSlide = $('.prev-slide');
+    var nextSlide = $('.next-slide');
+
     //powiększenie szerokości slidera
-    slideShow.css('width', slideCount*100 + '%');
-    
-    console.log(slideIndex);
-    
+    slideShow.css('width', slideCount * 100 + '%');
+
     //dodaj margines i szerokość
-    slideShow.find('single-slide').each(function(index){
-        
-        /* // sposón Łukasza
-        $(this).css({"width": slideWidth + "%", "left": index * slideWidth + "%"
-        */
-        $('this').css({'margin-left': slideWidth * index + '%', 'width': 100 / index + '%'
+    slideShow.find('.single-slide').each(function (index) {
+
+        // sposóbn Łukasza
+        $(this).css({
+            "width": slideWidth + "%",
+            "left": index * slideWidth + "%"
         });
     });
+
+    /*  Nie takkie dobre rozwiązanie - wywala inne wartości
+    $(this).css({'margin-left': slideWidth * index + '%', 'width': 100 / index + '%'
+        
+    });*/
+
+    
     
     //strzałki od sliderów
-    $('.prev-slide').onclick(function(){
+    prevSlide.click(function (e) {
         slide(slideIndex - 1);
-    }); 
-    
-    $('.next-slide').onclick(function(){
+        //console.log('<--klik')
+        e.preventDefault();
+    });
+ 
+    nextSlide.click(function (e) {
         slide(slideIndex + 1);
+        //console.log('klik -->')
+        e.preventDefault();
+
     });
     
+    
+    
+    // funkcja slide 
     function slide(newSlideIndex) {
-        if (newSlideIndex = 0 || newSlideIndex > slideCount){
+        console.log(newSlideIndex);
+        if (newSlideIndex < 0 || newSlideIndex >= slideCount) {
             return;
         }
         
-    }
+        var slideCaption = $('.slider-caption').eq(newSlideIndex);// eq wybieram index z array
+        
+        slideCaption.hide(); 
+        
+        var marginLeft = (newSlideIndex * -100) +'%'; 
+        
+        slideShow.animate({'margin-left': marginLeft}, 2000, function() {
+            slideIndex = newSlideIndex;
+            slideCaption.fadeIn('slow')
+        });
+    };
+
 });
